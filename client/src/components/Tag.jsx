737491567ed8527d177f32b6d2976ff20d4a9e4c
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-05-24 14:37:32 -0700

    Clean unneccssary files

--- client/src/components/Tag.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Tag = (props) => (
-  <div className="stock-chart-company-tags">
-    <span className="stock-chart-company-tags-name">{props.tag}</span>
-  </div>
-)
-
-export default Tag;
