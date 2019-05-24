b9d1999a28633e8825c867fcfbe919ec3c345c26
Gary <garyguan0713@gmail.com>
2019-05-24 15:48:13 -0700

    Add process.env.PORT

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/../public/dist')));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is now listening on port: ${port}`)
 })
 
